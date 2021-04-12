086398d5024b0048554b0f235eaf883bfcc604b9
Pinkmann <korgov.vova@icloud.com>
2021-04-12 14:11:01 +0300

    commit

--- src/common/Carousel/CarouselButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CarouselButton({side}) {
-    return (
-        <button className={"carousel__button carousel__button--" + side}>
-            <div className="arrow arrow--v2" />
-        </button>
-    )
-}
--- src/modules/case/components/Case.js
+++ src/modules/case/components/Case.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useEffect, useState } from 'react'
 
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import SwiperCore, { Navigation, Pagination, Swiper } from 'swiper/core';
 
 import Button from '../../../common/Button'
 import SideText from '../../../common/SideText'
 import bg from '../../../images/case/card.png'
+import itemImage from '../../../images/case/case-image.png';
 
 import Params from '../../../particlesParams/particles.json';
 import { Particles } from 'react-particles-js';
@@ -22,6 +22,37 @@ let gallery = [
 
 export default function Case() {
 
+    const [slides, setSlides] = useState({});
+
+    useEffect(() => {
+        const swiper = new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 40,
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'progressbar'
+            },
+            navigation: {
+                prevEl: '.swiper-button--left',
+                nextEl: '.swiper-button--right',
+            },
+            grabCursor: true,
+        });
+
+        setSlides({
+            total: swiper.slides.length,
+            current: 1
+        });
+
+
+        swiper.on('slideChange', () => {
+            setSlides({
+                total: swiper.slides.length,
+                current: ++swiper.realIndex
+            })
+        })
+    }, [])
+
     return (
         <section className="section case">
             <div className="title case__title container">
@@ -35,7 +66,7 @@ export default function Case() {
                     <ul className="case__list">
                         <li className="case__item">
                             <h3 className="case__item-title">Client:</h3>
-                            <img src="./images/case/case-image.png" alt="" className="case__item-image" />
+                            <img src={itemImage} alt="" className="case__item-image" />
                         </li>
                         <li className="case__item">
                             <h3 className="case__item-title">Accomplishments: </h3>
@@ -56,22 +87,30 @@ export default function Case() {
                     </ul>
                     <Button type='link' className='case__button' text='View Case study' textClass='button__text--blue' href='#' />
                 </div>
-                <Swiper
-                    spaceBetween={40}
-                    slidesPerView={1}
-                    pagination
-                    navigation
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
-                >
-                    {gallery.map(item => {
-                            return (
-                                <SwiperSlide className="carousel__item" key={item.id}>
-                                    <CaseCard item={item} />
-                                </SwiperSlide>
-                            );
-                        })}
-                </Swiper>
+                <div className="swiper">
+                    <div className="swiper-container">
+                        <ul className="swiper-wrapper">
+                            {gallery.map(item => {
+                                return (
+                                    <li className="swiper-slide" key={item.id}>
+                                        <CaseCard item={item} />
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <div className="swiper-pagination-wrapper">
+                        <span className="swiper-pagination-count swiper-pagination-count--left" id="current">{slides.current}</span>
+                        <span className="swiper-pagination"></span>
+                        <span className="swiper-pagination-count swiper-pagination-count--right" id="total">{slides.total}</span>
+                    </div>
+                    <button className="swiper-button swiper-button--left">
+                        <span className="arrow arrow--v2"></span>
+                    </button>
+                    <button className="swiper-button swiper-button--right">
+                        <span className="arrow arrow--v2"></span>
+                    </button>
+                </div>
             </div>
         </section>
 
